598dac5497fbdf1ca83cfbabfcd3e344dc0ef7b1
Jean-Baptiste Kempf <jb@videolan.org>
2026-07-04 11:43:09 +0200

    Fix unused warnings

--- src/navigation.c
+++ src/navigation.c
@@ -264,6 +264,8 @@ dvdnav_status_t dvdnav_part_play(dvdnav_t *this, int32_t title, int32_t part) {
 dvdnav_status_t dvdnav_part_play_auto_stop(dvdnav_t *this, int32_t title,
                                            int32_t part, int32_t parts_to_play) {
   /* FIXME: Implement auto-stop */
+  (void)parts_to_play;
+
  if (dvdnav_part_play(this, title, part) == DVDNAV_STATUS_OK)
    printerr("Not implemented yet.");
  return DVDNAV_STATUS_ERR;
@@ -272,6 +274,9 @@ dvdnav_status_t dvdnav_part_play_auto_stop(dvdnav_t *this, int32_t title,
 dvdnav_status_t dvdnav_time_play(dvdnav_t *this, int32_t title,
                                  uint64_t time) {
   /* FIXME: Implement */
+  (void)title;
+  (void)time;
+
   printerr("Not implemented yet.");
   return DVDNAV_STATUS_ERR;
 }
